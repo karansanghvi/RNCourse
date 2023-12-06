7d8410ba70433345a76d0ec94bb3fe70de804361
Karan Sanghvi <karansanghvi1303@gmail.com>
2023-12-06 20:33:58 +0530

    button

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Text>hello world</Text>
+      <View>
+        <Text>this is another tex</Text>
+      </View>
+      <Button title = "click me" />
     </View>
   );
 }
