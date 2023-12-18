e6b751894e700a4a24ccc1e5ba6b7cea832611b5
Karan Sanghvi <karansanghvi1303@gmail.com>
2023-12-18 15:52:50 +0530

    lec 105 over

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,52 @@
-import { StyleSheet, View, Text } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { useLayoutEffect } from "react";
+import { StyleSheet, View, FlatList } from "react-native";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({route, navigation}) {
+
+    const catId = route.params.categoryId;
+    
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    });
+
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find(
+            (category) => category.id === catId
+        ).title;
+
+        navigation.setOptions({
+            title: categoryTitle,
+        });
+    }, [catId, navigation]);
+
+    function renderMealItem(itemData) {
+
+        const item = itemData.item;
+        const mealItemProps = {
+            id: item.id,
+            title: item.title,
+            imageUrl: item.imageUrl,
+            duration: item.duration,
+            complexity: item.complexity,
+            affordability: item.affordability
+        };
+
+        return (
+            <MealItem 
+                {...mealItemProps}
+            />
+        );
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Meals Overview Screen</Text>
+            <FlatList 
+                data={displayedMeals} 
+                keyExtractor={(item) => item.id} 
+                renderItem={renderMealItem} 
+            />
         </View>
     );
 }
