f56706582dc2647fe64a134a574a676d361de36c
Karan Sanghvi <karansanghvi1303@gmail.com>
2023-12-19 15:13:54 +0530

    new branch

--- App.js
+++ App.js
@@ -1,52 +1,9 @@
-import { StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import CategoriesScreen from './screens/CategoriesScreen';
-import MealsOverviewScreen from './screens/MealsOverviewScreen';
-import MealDetailScreen from './screens/MealDetailsScreen';
+import { Text } from "react-native";
 
-const Stack = createStackNavigator();
-
-export default function App() {
+function App() {
   return (
-    <>
-      <StatusBar style="light"/>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerStyle:{ backgroundColor: '#351401' },
-          headerTintColor: 'white',
-          contentStyle: {backgroundColor: '#3f2f25'},
-        }}>
-          <Stack.Screen 
-            name="MealsCategories" 
-            component={CategoriesScreen} 
-            options={{
-              title: 'All Categories',
-            }} 
-          />
-          <Stack.Screen 
-            name="MealsOverview" 
-            component={MealsOverviewScreen} 
-            // options = {({route, navigation}) => {
-            //   const catId = route.params.categoryId;
-            //   return {
-            //     title: catId,
-            //   };
-            // }}
-          />
-          <Stack.Screen 
-            name="MealDetail" 
-            component={MealDetailScreen}
-            // options={{
-            //   headerRight: () => {
-            //     return (
-            //       <Button title="Tap Me" />
-            //     );
-            //   }
-            // }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <Text>To do</Text>
   );
 }
+
+export default App;
