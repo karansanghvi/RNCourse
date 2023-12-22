75f88e506de8247f435eaaabf2095a1146c3e69d
Karan Sanghvi <karansanghvi1303@gmail.com>
2023-12-22 14:47:10 +0530

    lec 132 completed

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -2,11 +2,49 @@ import { View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput({expenses}) {
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 69.99,
+        date: new Date('2021-12-19')
+    },
+    {
+        id: 'e2',
+        description: 'A pair of trousers',
+        amount: 109.99,
+        date: new Date('2023-9-20')
+    },
+    {
+        id: 'e3',
+        description: 'Some bananas',
+        amount: 99.99,
+        date: new Date('2022-12-01')
+    },
+    {
+        id: 'e4',
+        description: 'A book',
+        amount: 4.96,
+        date: new Date('2021-10-29')
+    },
+    {
+        id: 'e5',
+        description: 'A watch',
+        amount: 21.99,
+        date: new Date('2023-03-04')
+    }
+]
+
+function ExpensesOutput({expenses, expensesPeriod}) {
     return (
         <View>
-            <ExpensesSummary/>
-            <ExpensesList/>
+            <ExpensesSummary
+                periodName={expensesPeriod}
+                expenses={DUMMY_EXPENSES}
+            />
+            <ExpensesList
+                expenses={DUMMY_EXPENSES}
+            />
         </View>
     );
 }
--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,8 +1,10 @@
-import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function AllExpenses() {
     return (
-        <Text>AllExpenses Screen</Text>
+        <ExpensesOutput
+            expensesPeriod="Total"
+        />
     );
 }
 
